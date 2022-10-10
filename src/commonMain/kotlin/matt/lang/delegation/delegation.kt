c2bcc270c41fcb7534fb050ac65d94fc73078f85
mgroth0 <mgroth49@gmail.com>
2022-10-10 00:20:24 -0400

    autocommit

---
+++ src/commonMain/kotlin/matt/lang/delegation/delegation.kt
@@ -0,0 +1,29 @@
+package matt.lang.delegation
+
+import kotlin.properties.PropertyDelegateProvider
+import kotlin.properties.ReadOnlyProperty
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+fun <V, P: ReadOnlyProperty<Any?, V>> provider(
+  provideDelegate: (pName: String)->P
+) = PropertyDelegateProvider { _: Any?, property -> provideDelegate(property.name) }
+
+fun <V> valProp(
+  op: ()->V
+) = ReadOnlyProperty { _: Any?, _ -> op() }
+
+fun <V> varProp(
+  getter: ()->V,
+  setter: (V)->Unit
+) = object: ReadWriteProperty<Any?, V> {
+
+  override fun getValue(thisRef: Any?, property: KProperty<*>): V {
+	return getter()
+  }
+
+  override fun setValue(thisRef: Any?, property: KProperty<*>, value: V) {
+	setter(value)
+  }
+
+}
