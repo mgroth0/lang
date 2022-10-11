9df2e82b6227ced2e283d6ae2215e29f6b3b8623
mgroth0 <mgroth49@gmail.com>
2022-10-11 16:59:54 -0400

    autocommit

--- src/jsMain/kotlin/matt/lang/weak/weak.kt
+++ src/jsMain/kotlin/matt/lang/weak/weak.kt
@@ -1,6 +1,6 @@
 package matt.lang.weak
 
-actual external class WeakRef<T: Any> actual constructor(value: T?) {
+actual external class WeakRef<T: Any> actual constructor(value: T) {
   actual fun deref(): T?
 }
 
