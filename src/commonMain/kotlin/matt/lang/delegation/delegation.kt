5ac03f3e5ec22af398a067d9888a2643a2d5308b
mgroth0 <mgroth49@gmail.com>
2022-10-18 11:40:42 -0400

    autocommit

--- src/commonMain/kotlin/matt/lang/delegation/delegation.kt
+++ src/commonMain/kotlin/matt/lang/delegation/delegation.kt
@@ -5,6 +5,7 @@ import kotlin.properties.ReadOnlyProperty
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
+/*
 @Suppress("UnusedReceiverParameter")
 fun <R, V, P: ReadOnlyProperty<R, V>> R.provider(
   provideDelegate: (pName: String)->P
@@ -36,6 +37,36 @@ fun <R, V> varProp(
   }
 
   override fun setValue(thisRef: R, property: KProperty<*>, value: V) {
+	setter(value)
+  }
+
+}
+*/
+
+
+
+fun <V, P: ReadOnlyProperty<Any?, V>> provider(
+  provideDelegate: (pName: String)->P
+) = PropertyDelegateProvider { _: Any?, property -> provideDelegate(property.name) }
+
+fun <V, P: ReadOnlyProperty<Any?, V>> fullProvider(
+  provideDelegate: (thisRef: Any?, prop: KProperty<*>)->P
+) = PropertyDelegateProvider { thisRef: Any?, property -> provideDelegate(thisRef, property) }
+
+fun <V> valProp(
+  op: ()->V
+) = ReadOnlyProperty { _: Any?, _ -> op() }
+
+fun <V> varProp(
+  getter: ()->V,
+  setter: (V)->Unit
+) = object: ReadWriteProperty<Any?, V> {
+
+  override fun getValue(thisRef: Any?, property: KProperty<*>): V {
+	return getter()
+  }
+
+  override fun setValue(thisRef: Any?, property: KProperty<*>, value: V) {
 	setter(value)
   }
 
