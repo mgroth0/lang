ab8e85381dc365db146df2d31c16dfb7b379bad2
mgroth0 <mgroth49@gmail.com>
2022-10-21 16:30:50 -0400

    autocommit

--- src/jvmMain/kotlin/matt/lang/sync/sync.kt
+++ src/jvmMain/kotlin/matt/lang/sync/sync.kt
@@ -1,7 +1,8 @@
 @file:JvmName("SyncKtJvm")
+
 package matt.lang.sync
 
-actual fun <R> inSync(monitor: Any, op: ()->R): R {
+actual inline fun <R> inSync(monitor: Any, op: ()->R): R {
   return synchronized(monitor) {
 	op()
   }
