9e3ca9ef3ad18a57b5c96b2a6914ce275bf17650
mgroth0 <mgroth49@gmail.com>
2022-10-22 23:27:25 -0400

    autocommit

--- src/commonMain/kotlin/matt/lang/safeconvert/safeconvert.kt
+++ src/commonMain/kotlin/matt/lang/safeconvert/safeconvert.kt
@@ -4,3 +4,8 @@ fun Long.requireIsByte(): Byte {
   require(this >= Byte.MIN_VALUE && this <= Byte.MAX_VALUE)
   return toByte()
 }
+
+fun Long.requireIsInt(): Int {
+  require(this >= Int.MIN_VALUE && this <= Int.MAX_VALUE)
+  return toInt()
+}
