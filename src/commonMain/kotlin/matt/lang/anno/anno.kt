77e576f27cb623e5c0a2ca8aa2e54e9bbe7fc3ad
mgroth0 <mgroth49@gmail.com>
2022-11-08 13:20:43 -0500

    autocommit

--- src/commonMain/kotlin/matt/lang/anno/anno.kt
+++ src/commonMain/kotlin/matt/lang/anno/anno.kt
@@ -1,14 +1,19 @@
 package matt.lang.anno
 
+import kotlin.annotation.AnnotationRetention.SOURCE
 import kotlin.annotation.AnnotationTarget.CLASS
+import kotlin.annotation.AnnotationTarget.EXPRESSION
+import kotlin.annotation.AnnotationTarget.FILE
 import kotlin.annotation.AnnotationTarget.FUNCTION
+import kotlin.annotation.AnnotationTarget.LOCAL_VARIABLE
+import kotlin.annotation.AnnotationTarget.TYPEALIAS
 import kotlin.reflect.KClass
 
 annotation class GeneralizationCandidate
 annotation class Recycle
 annotation class NeedsTest
 
-@Target(AnnotationTarget.TYPEALIAS)
+@Target(TYPEALIAS)
 annotation class TemporaryCode
 annotation class ExperimentalCode
 annotation class DoesNotAlwaysWork
@@ -16,7 +21,17 @@ annotation class Duplicated
 annotation class NullToReduceObjects
 annotation class MergeWith(vararg val classes: KClass<*>)
 
-@Target(AnnotationTarget.FILE, CLASS, FUNCTION)
+@Target(FILE, CLASS, FUNCTION)
 /*I would like this to be AnnotationTarget.ANY but that does not seem to exist*/
 annotation class See(val link: KClass<*>)
+
+@Target(EXPRESSION, LOCAL_VARIABLE)
+@Retention(SOURCE) /*needs to be source to be allowed on expression*/
+@Repeatable
 annotation class SeeURL(val url: String)
+
+
+@Target(EXPRESSION, LOCAL_VARIABLE)
+@Retention(SOURCE) /*needs to be source to be allowed on expression*/
+@Repeatable
+annotation class SeeURLs(vararg val url: String)
