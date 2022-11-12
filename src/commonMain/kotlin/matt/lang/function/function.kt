06582d88cb032b3241961706f7a27681015b5cd3
mgroth0 <mgroth49@gmail.com>
2022-11-11 19:35:55 -0500

    autocommit

--- src/commonMain/kotlin/matt/lang/function/function.kt
+++ src/commonMain/kotlin/matt/lang/function/function.kt
@@ -3,6 +3,8 @@ package matt.lang.function
 typealias Op = ()->Unit
 typealias Produce<T> = ()->T
 typealias Consume<T> = On<T>
+typealias Convert<I,O> = (I) -> O
+typealias Mutate<T> = (T) -> T
 typealias Pred = Produce<Boolean>
 typealias On<T> = (T)->Unit
 typealias DSL<T> = T.()->Unit
--- src/commonMain/kotlin/matt/lang/safeconvert/safeconvert.kt
+++ src/commonMain/kotlin/matt/lang/safeconvert/safeconvert.kt
@@ -1,11 +1,15 @@
 package matt.lang.safeconvert
 
 fun Long.requireIsByte(): Byte {
-  require(this >= Byte.MIN_VALUE && this <= Byte.MAX_VALUE)
+  require(this >= Byte.MIN_VALUE && this <= Byte.MAX_VALUE) {
+    "${this} is not a byte"
+  }
   return toByte()
 }
 
 fun Long.requireIsInt(): Int {
-  require(this >= Int.MIN_VALUE && this <= Int.MAX_VALUE)
+  require(this >= Int.MIN_VALUE && this <= Int.MAX_VALUE) {
+    "${this} is not an int"
+  }
   return toInt()
 }
