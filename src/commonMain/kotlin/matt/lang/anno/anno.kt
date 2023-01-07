dd1cb362e6e2e67c477e071134ae629a03842404
mgroth0 <mgroth49@gmail.com>
2023-01-07 13:49:53 -0500

    autocommit

--- src/commonMain/kotlin/matt/lang/anno/anno.kt
+++ src/commonMain/kotlin/matt/lang/anno/anno.kt
@@ -1,12 +1,7 @@
 package matt.lang.anno
 
 import kotlin.annotation.AnnotationRetention.SOURCE
-import kotlin.annotation.AnnotationTarget.CLASS
-import kotlin.annotation.AnnotationTarget.EXPRESSION
-import kotlin.annotation.AnnotationTarget.FILE
-import kotlin.annotation.AnnotationTarget.FUNCTION
-import kotlin.annotation.AnnotationTarget.LOCAL_VARIABLE
-import kotlin.annotation.AnnotationTarget.TYPEALIAS
+import kotlin.annotation.AnnotationTarget.*
 import kotlin.reflect.KClass
 
 annotation class GeneralizationCandidate
@@ -27,7 +22,15 @@ annotation class MergeWith(vararg val classes: KClass<*>)
 /*I would like this to be AnnotationTarget.ANY but that does not seem to exist*/
 annotation class See(val link: KClass<*>)
 
-@Target(EXPRESSION, LOCAL_VARIABLE, CLASS, AnnotationTarget.CLASS, AnnotationTarget.PROPERTY, AnnotationTarget.FUNCTION)
+@Target(
+  EXPRESSION,
+  LOCAL_VARIABLE,
+  CLASS,
+  CLASS,
+  PROPERTY,
+  FUNCTION,
+  FILE
+)
 @Retention(SOURCE) /*needs to be source to be allowed on expression*/
 @Repeatable
 annotation class SeeURL(val url: String)
